6454b14e2af5fc49afc25b6ac095cce447975dc3
balaji10krishnan <balaji10krishnan@gmail.com>
2024-10-03 14:04:39 +0530

    create budget and add expense

--- src/context/BudjetContext.js
+++ src/context/BudjetContext.js
@@ -1,11 +1,44 @@
 import { createContext } from "react";
 import useLocalStorage from "../hook/use-localstorage";
-
+import { v4 as uuidv4 } from "uuid";
 export const BudjetContext = createContext();
 const BudjetContextProvider = ({ children }) => {
   const [userName, setUserName] = useLocalStorage("userName", null);
+  const [budgets, setBudgets] = useLocalStorage("budgets", []);
+  const [expenses, setExpenses] = useLocalStorage("expenses", []);
+
+  const generateRandomColor = (val) => {
+    return `${val * 34} 55% 40%`;
+  };
+
+  const addBudgets = (budget, cost) => {
+    setBudgets([
+      ...budgets,
+      {
+        id: uuidv4(),
+        budget,
+        cost,
+        color: generateRandomColor(budgets.length),
+      },
+    ]);
+  };
+
+  const addExpense = (budgetId, expense, cost) => {
+    setExpenses([
+      ...expenses,
+      {
+        id: uuidv4(),
+        expense,
+        cost,
+        budgetId,
+      },
+    ]);
+  };
+
   return (
-    <BudjetContext.Provider value={{ userName, setUserName }}>
+    <BudjetContext.Provider
+      value={{ userName, setUserName, addBudgets, budgets, addExpense }}
+    >
       {children}
     </BudjetContext.Provider>
   );
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,4 +1,19 @@
+import { useContext } from "react";
+import AddExpense from "../../features/AddExpense/AddExpense";
+import CreateBudget from "../../features/CreateBudget/CreateBudget";
+import Greet from "../../features/Greet/Greet";
+import classes from "./Dashboard.module.css";
+import { BudjetContext } from "../../context/BudjetContext";
 const Dashboard = () => {
-  return <p>Dashboard</p>;
+  const { budgets } = useContext(BudjetContext);
+  return (
+    <section className={classes["dashboard-container"]}>
+      <Greet />
+      <div className={classes["grid-item"]}>
+        <CreateBudget />
+        {budgets.length > 0 && <AddExpense />}
+      </div>
+    </section>
+  );
 };
 export default Dashboard;
